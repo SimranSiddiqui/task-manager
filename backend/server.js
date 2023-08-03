8464fb8cb65efc8c2e0b5e4503fe3bac148cea6d
Simran Siddiqui <simransidd005@gmail.com>
2023-08-03 23:21:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const cors = require('cors');
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(
+    cors({
+    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 app.use('/api/tasks', taskRoutes);
